ed7a6573731b78fc9e420d6092cf35253017fe66
DNaimji <d.naimji@mail.utoronto.ca>
2014-10-17 22:49:47 -0400

    Update breakout.js

--- breakout.js
+++ breakout.js
@@ -1,3 +1,40 @@
 (function(){
+  window.onload = init;
+	
+	var canvas;
+	var context;
 
+	var posX;
+	var posY;
+	var width;
+	var height;
+	var dx = 2;
+	var dy = 4;
+
+	function init() {
+	    $("#new_game").bind("click", game_init);
+	}
+	 
+	function game_init() {
+
+	    canvas = $("#canvas");
+	    context = canvas[0].getContext("2d");
+	    width = canvas.width();
+	    height = canvas.height();
+	    posY = height * 0.5;
+	    posX = width * 0.5;
+
+	    return setInterval(draw, 100);
+	}
+	 
+	function draw() {
+	    context.clearRect(0, 0, height, width);
+	    context.beginPath();
+	    context.rect(posX, posY, 10, 10);
+	    context.closePath();
+	    context.fill();
+	    posX += dx;
+	    posY += dy;
+	}
+	
 })();
